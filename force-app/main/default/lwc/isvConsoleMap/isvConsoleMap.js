09365646baab2150ddbbe33605296b7f84637dfb
rreboucas <roreb13@gmail.com>
2020-04-15 13:17:44 -0400

    added map controller apex

--- force-app/main/default/lwc/isvConsoleMap/isvConsoleMap.js
+++ force-app/main/default/lwc/isvConsoleMap/isvConsoleMap.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
  * Copyright (c) 2018, salesforce.com, inc.
  * All rights reserved.
@@ -34,10 +35,17 @@ const ACCT_FIELDS = [
 ];
 
 export default class IsvConsoleMap extends NavigationMixin(LightningElement) {
+=======
+import { LightningElement, track, api, wire } from 'lwc';
+import getLocationData from '@salesforce/apex/isvConsoleMapController.getLocationData';
+
+export default class IsvConsoleMap extends LightningElement {
+>>>>>>> 033b27d (added map controller apex)
     
     @api title;
     mapMarkers = [];
     error;
+<<<<<<< HEAD
     numberOfMarkers;
     isMobile = false;
     showFooter = false;
@@ -101,19 +109,28 @@ export default class IsvConsoleMap extends NavigationMixin(LightningElement) {
          
     }
 
+=======
+    
+>>>>>>> 033b27d (added map controller apex)
     @wire(getLocationData)
     wiredLocations({ error, data }) {
         if (data) {
             this.mapMarkers = data;
+<<<<<<< HEAD
             this.error = 'No Data found';
             this.numberOfMarkers = data.length;
         }
         else if (error) {
+=======
+            this.error = undefined;
+        } else if (error) {
+>>>>>>> 033b27d (added map controller apex)
             this.error = error;
             this.mapMarkers = undefined;
         }
     }
     
+<<<<<<< HEAD
     
     @track markersTitle = "My Customers";
 
@@ -243,4 +260,115 @@ export default class IsvConsoleMap extends NavigationMixin(LightningElement) {
     }
 
 
+=======
+    /*
+    mapMarkers = [
+        {
+            value: 'US1',
+            location: {
+                State: "Texas",
+                Country: 'USA',
+            },
+
+            icon: 'custom:custom26',
+            title: "US1",
+        },
+        {
+            value: 'US2',
+            location: {
+                State: 'Florida',
+                Country: 'US',
+            },
+
+            icon: 'custom:custom96',
+            title: 'US2',
+        },
+        {
+            value: 'France3',
+            location: {
+                City: 'Saint-Jean-Cap-Ferrat',
+                Country: 'France',
+            },
+
+            title: 'Saint-Jean-Cap-Ferrat',
+        },
+        {
+            value: 'France4',
+            location: {
+                City: 'Villefranche-sur-Mer',
+                Country: 'France',
+            },
+
+            icon: 'custom:custom92',
+            title: 'Villefranche-sur-Mer',
+        },
+        {
+            value: 'France5',
+            location: {
+                City: 'Antibes',
+                Country: 'France',
+            },
+
+            icon: 'custom:custom61',
+            title: 'Antibes',
+        },
+        {
+            value: 'France6',
+            location: {
+                City: 'Juan-les-Pins',
+                Country: 'France',
+            },
+
+            icon: 'custom:custom74',
+            title: 'Juan-les-Pins',
+        },
+        {
+            value: 'France7',
+            location: {
+                City: 'Cannes',
+                Country: 'France',
+            },
+
+            icon: 'custom:custom3',
+            title: 'Cannes',
+        },
+        {
+            value: 'France8',
+            location: {
+                City: 'Saint-Raphaël',
+                Country: 'France',
+            },
+
+            icon: 'custom:custom54',
+            title: 'Saint-Raphaël',
+        },
+        {
+            value: 'France9',
+            location: {
+                City: 'Fréjus',
+                Country: 'France',
+            },
+
+            icon: 'custom:custom88',
+            title: 'Fréjus',
+        },
+        {
+            value: 'France10',
+            location: {
+                City: 'Sainte-Maxime',
+                Country: 'France',
+            },
+
+            icon: 'custom:custom92',
+            title: 'Sainte-Maxime',
+        },
+    ]; */
+    @track markersTitle = "My Customers";
+
+    @track selectedMarkerValue = 'France1';
+
+    handleMarkerSelect(event) {
+        this.selectedMarkerValue = event.detail.selectedMarkerValue;
+    }
+>>>>>>> 033b27d (added map controller apex)
 }
