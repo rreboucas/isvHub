b024a8c16818f904148c6b6ec29d9e2a62f12a5e
HoltMichael <michaelholt@hotmail.co.uk>
2021-01-17 19:49:34 +0000

    UI updates and Lightning Flow added

--- force-app/main/default/lwc/countdownTimer/countdownTimer.js
+++ force-app/main/default/lwc/countdownTimer/countdownTimer.js
@@ -5,14 +5,17 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import getExpirationDate from '@salesforce/apex/CountdownTimerController.getExpirationDate';
 
 export default class CountdownTimer extends LightningElement {
     title = "Org Expiration"
     deadline;
-    deadlineStr;
     timeRemainingAsString;
+    expanded = false;
+
+    @track daysBeforeExpiry;
+    @track buttonLabel = "Configure Expiry Notifications";
     
     connectedCallback(){
         this.getOrgExpiration();
@@ -37,20 +40,25 @@ export default class CountdownTimer extends LightningElement {
     }
 
     getTimeRemaining(){
-        console.log('Getting time in function...');
         const total = Date.parse(this.deadline) - Date.parse(new Date());
         const seconds = Math.floor( (total/1000) % 60 );
         const minutes = Math.floor( (total/1000/60) % 60 );
         const hours = Math.floor( (total/(1000*60*60)) % 24 );
-        const days = Math.floor( total/(1000*60*60*24) );
-        
-        
+        const days = Math.floor( total/(1000*60*60*24) );     
 
         this.timeRemainingAsString =  days + ' days ' + hours + ' hours ' +  minutes + ' minutes ' + seconds + ' seconds';
         
         var deadlineDate = new Date(this.deadline); 
-        this.deadlineStr = deadlineDate.toDateString();
-        
-        console.log(this.timeRemainingAsString);
+        this.deadline = deadlineDate.toDateString();
+        this.daysBeforeExpiry = days;
+      }
+
+      invertExpanded(){
+          this.expanded = !this.expanded;
+          if(this.expanded){
+            this.buttonLabel = "Collapse";
+          }else{
+            this.buttonLabel = "Configure Expiry Notifications";
+          }
       }
 }
